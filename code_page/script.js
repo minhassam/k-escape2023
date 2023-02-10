08fa3293a941329aaec7bb3dea29ee8a9ef79d27
minhassam <kimmh0713@naver.com>
2023-02-10 21:39:26 +0900

    수정

--- code_page/script.js
+++ code_page/script.js
@@ -82,4 +82,44 @@ function to2() {
 	});
 
 }
+function to3() {
+	let input = document.getElementById("input");
+	let error = document.getElementById("errorname");
+	let check = document.getElementById("check");
+	
+	//시작 전 경고 문구와 애니메이션 초기화
+	error.innerHTML = "";
+	input.style.animation='';
 
+	function ans()
+	{
+		if(input.value == "세종대왕")
+			location.href="./page03_한글사랑.html";
+		else
+		{
+			input.style.animation='shake 0.5s';
+			error.innerHTML = "다시 풀어보세요";
+		}
+	}
+
+	//타이핑을 했을 때
+	input.addEventListener("keydown", e => {
+		//엔터라면 답안 확인(ans 함수 호출)
+		if(e.keyCode === 13) {
+			ans();
+		}
+	});
+
+	//입력창을 클릭했을 때
+	input.addEventListener("click", () => {
+		error.innerHTML = "";
+		input.style.animation='';
+	});
+
+
+	//확인 버튼을 클릭했을 때
+	check.addEventListener("click", () => {
+		ans();
+	});
+
+}
